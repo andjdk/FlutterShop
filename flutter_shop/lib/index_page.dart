9984ca4bea745468a5058e00e1301149a8fd89ba
andjdk <353010666@qq.com>
2019-05-30 00:41:30 +0800

    首页目录增加

--- flutter_shop/lib/index_page.dart
+++ flutter_shop/lib/index_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_shop/pages/cart_page.dart';
 import 'package:flutter_shop/pages/category_page.dart';
 import 'package:flutter_shop/pages/home_page.dart';
@@ -36,6 +37,7 @@ class _IndexPageState extends State<IndexPage> {
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750,height: 1334)..init(context);
     return Scaffold(
       backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
       bottomNavigationBar: BottomNavigationBar(
--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_shop/service_method/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -18,6 +19,22 @@ class _HomePageState extends State<HomePage> {
     'http://static6.eloancn.com/images/pic_14.jpg?eloancnV=201405281828',
   ];
 
+
+  List navigatorList=[
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+  ];
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,25 +45,29 @@ class _HomePageState extends State<HomePage> {
             future: getHomePageContent(),
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                try{
+                try {
                   var data = json.decode(snapshot.data.toString());
                   List<Map> swiper = (data['data']['slides'] as List).cast();
+//                  List<Map> navigatorList = (data['data']['category'] as List).cast();
                   return Column(
                     children: <Widget>[
-                      SwiperDiy(swiperDateList:banners),
+                      SwiperDiy(swiperDateList: banners),
+                      TopNavigator(navigatorList: navigatorList,)
                     ],
                   );
-                }catch (e){
+                } catch (e) {
                   return Column(
                     children: <Widget>[
-                      SwiperDiy(swiperDateList:banners),
+                      SwiperDiy(swiperDateList: banners),
+                      TopNavigator(navigatorList: navigatorList,)
                     ],
                   );
                 }
-              }else{
+              } else {
                 return Column(
                   children: <Widget>[
-                    SwiperDiy(swiperDateList:banners),
+                    SwiperDiy(swiperDateList: banners),
+                    TopNavigator(navigatorList: navigatorList,)
                   ],
                 );
               }
@@ -62,14 +83,60 @@ class SwiperDiy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 150,
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
       child: Swiper(
         itemBuilder: (BuildContext context, int index) {
-          return Image.network(swiperDateList[index],fit: BoxFit.fill,);
+          return Image.network(
+            swiperDateList[index],
+            fit: BoxFit.fill,
+          );
         },
         itemCount: swiperDateList.length,
         pagination: SwiperPagination(),
         autoplay: true,
+      ),
+    );
+  }
+}
+
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+
+  const TopNavigator({Key key, this.navigatorList}) : super(key: key);
+
+  Widget _gridViewItemUI(BuildContext context, item) {
+    return InkWell(
+      onTap: () {
+
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+//          Image.network(
+//            item['image'],
+//            width: ScreenUtil().setWidth(95),
+//            fit: BoxFit.fill,
+//          ),
+//          Text(item['mallCategoryName']),
+          Icon(Icons.favorite,),
+          Text(item.toString())
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding: EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(5),
+        children: navigatorList.map((item) {
+          return _gridViewItemUI(context, item);
+        }).toList(),
       ),
     );
   }
