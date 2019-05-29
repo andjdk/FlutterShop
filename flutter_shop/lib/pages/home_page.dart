64377ede0cc1bc1dc946776508ece534858ae1ab
雄亮 <txl@hxdl.com>
2019-05-29 18:34:29 +0800

    首页增加banner 及请求接口

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -1,12 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_shop/service_method/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> banners = [
+    'http://picapi.zhituad.com/photo/05/48/63ABA.jpg',
+    'http://picapi.zhituad.com/photo/05/48/63ABA.jpg',
+    'http://static6.eloancn.com/images/pic_14.jpg?eloancnV=201405281828',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("首页"),),
+        appBar: AppBar(
+          title: Text("百姓生活+"),
+        ),
+        body: FutureBuilder(
+            future: getHomePageContent(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                try{
+                  var data = json.decode(snapshot.data.toString());
+                  List<Map> swiper = (data['data']['slides'] as List).cast();
+                  return Column(
+                    children: <Widget>[
+                      SwiperDiy(swiperDateList:banners),
+                    ],
+                  );
+                }catch (e){
+                  return Column(
+                    children: <Widget>[
+                      SwiperDiy(swiperDateList:banners),
+                    ],
+                  );
+                }
+              }else{
+                return Column(
+                  children: <Widget>[
+                    SwiperDiy(swiperDateList:banners),
+                  ],
+                );
+              }
+            }));
+  }
+}
+
+class SwiperDiy extends StatelessWidget {
+  final List swiperDateList;
+
+  SwiperDiy({this.swiperDateList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(swiperDateList[index],fit: BoxFit.fill,);
+        },
+        itemCount: swiperDateList.length,
+        pagination: SwiperPagination(),
+        autoplay: true,
+      ),
     );
   }
-
 }
---
+++ flutter_shop/lib/service_method/service_method.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'dart:async';
+import 'dart:io';
+import '../config/service_url.dart';
+
+//获取首页主题内容
+Future getHomePageContent() async{
+  try{
+    print("开始获取首页数据......");
+    Response response;
+    Dio dio = Dio();
+    dio.options.contentType = ContentType.parse("application/x-www-form-urlencoded");
+    var formData = {'lon':'115.02932','lat':'35.76189'};
+    response = await dio.post(servicePath['homePageContent'],data: formData);
+    if(response.statusCode == 200){
+      return response.data;
+    }else{
+      return Exception("后端接口出现异常");
+    }
+  }catch (e){
+    return "error：==========> $e";
+  }
+}
