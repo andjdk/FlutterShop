d312a4b45a01d797c13657e24eb75a0350031753
雄亮 <txl@hxdl.com>
2019-05-29 16:17:01 +0800

    增加底部导航

--- flutter_shop/lib/index_page.dart
+++ flutter_shop/lib/index_page.dart
@@ -1,17 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_shop/pages/cart_page.dart';
+import 'package:flutter_shop/pages/category_page.dart';
+import 'package:flutter_shop/pages/home_page.dart';
+import 'package:flutter_shop/pages/member_page.dart';
 
-class IndexPage extends StatelessWidget{
+class IndexPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _IndexPageState();
+  }
+}
+
+class _IndexPageState extends State<IndexPage> {
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), title: Text("首页")),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.search), title: Text("分类")),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.shopping_cart), title: Text("购物车")),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.profile_circled), title: Text("会员中心")),
+  ];
+
+  final List tabBodies = [HomePage(), CategoryPage(), CartPage(), MemberPage()];
+
+  int currentIndex = 0;
+  var currentPage;
+
+  @override
+  void initState() {
+    super.initState();
+    currentPage = tabBodies[currentIndex];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("百姓生活家"),
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items: bottomTabs,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            currentPage = tabBodies[currentIndex];
+          });
+        },
       ),
-      body: Center(
-        child: Text("百姓生活家"),
-      ),
+      body: currentPage,
     );
   }
-
-
 }
